5e8df3e4fdc3341c2f52bd7d9ff5c37f2a5a6ff6
Anucha M <monopeelz@gmail.com>
2021-09-04 17:08:51 +0700

    update sql query

--- exam_3.sql
+++ exam_3.sql
@@ -1,20 +1,23 @@
 -- Exam 3.1
-select e.name, COUNT(eh.employee_id) as e_count from employee e
+select e.name, COUNT(e.employee_id) as hobby_count from employee e
 join employee_hobby eh on e.employee_id = eh.employee_id
-where e_count > 1
+group by e.name
+HAVING hobby_count > 1;
 
 -- Exam 3.2
-select e.name, COUNT(eh.employee_id) as e_manhour from employee e
+select e.name, (ew.salary DIV ew.total_hour) as manhour from employee e
 join employee_work ew on e.employee_id = ew.employee_id
-where e_manhour >= 350
+group by e.name, ew.salary, ew.total_hour
+HAVING manhour >= 350;
 
 -- Exam 3.3
-select eh.name, COUNT(eh.employee_id) as e_manhour from employee e
+select eh.name, COUNT(eh.employee_id) as person from employee e
 join employee_hobby eh on e.employee_id = eh.employee_id
 join employee_work ew on e.employee_id = ew.employee_id
 where ew.perf > 70
+group by eh.name;
 
 -- Exam 3.4
 select e.name, e.lastname from employee e
 join employee_work ew on e.employee_id = ew.employee_id
-where ew.perf > 80 and e.age > 25
+where ew.perf > 80 and e.age > 25;
